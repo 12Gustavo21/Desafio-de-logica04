4df15ae8d397916b2a779662b5e63f46ae7a2e38
12Gustavo21 <gustavo927318@gmail.com>
2022-06-18 12:25:00 -0300

    Desafio-de-logica04

---
+++ script.js
@@ -0,0 +1,34 @@
+//Questão 01
+function mensagem() {
+    console.log("Aprecie os gatinhos, cute cute");
+}
+
+mensagem();
+
+//Questão 02
+function nome(meuNome){
+    console.log(`Meu nome é ${meuNome}`);
+}
+    
+nome("Gustavo");
+
+//Questão 03
+function about(myName, idade, estiloMusical) {
+    console.log(`Meu nome é ${myName}, minha idade é ${idade} e meu estilo musical é ${estiloMusical}`);
+}
+    
+about("Gustavo", 17, "Pop e gospel");
+
+//Questão 04
+function abou1(movie, music) {
+    console.log(`Meu filme favorito é ${movie} e minha música favorita é ${music}`);
+}
+
+abou1("Intelestelar", "The perfect grill");
+
+//Questão 05
+function bonus(conta){
+    return conta * 3;
+}
+
+console.log(bonus(10));
